9159ba0ec9692e975dee93b8f0adaf5fba7f2660
LSDIZZLE <124414440+LSDIZZLE@users.noreply.github.com>
2023-02-03 22:14:19 -0700

    test

--- test.gd
+++ test.gd
@@ -1,13 +1,6 @@
+extends Node2D
+var newplatform=preload("res://Platform_test.tscn")
 func createNewGoal():
-	var newGoalNode = newGoal.instance()
-	newGoalNode.position = Vector2(get_viewport().get_mouse_position().x, get_viewport().get_mouse_position().y)
-	self.add_child(newGoalNode)
-	#newGoalBool = false
-	newGoalNode.message = map_i
-	newGoalNode.index = item_i
-	newGoalNode.connect("clicked", self, "_on_item_clicked")
-	goalDisplay.position = Vector2(original_positions[0], original_positions[1])
-	itemsArray.append(newGoalNode)
-	mapArray.append ( 1)
-	mapArray.append (newGoalNode.position.x )
-	mapArray.append (newGoalNode.position.y )
+	var newplatformNode = newplatform.instance()
+	newplatformNode.position = Vector2 (0,0)
+	self.add_child(newplatformNode)

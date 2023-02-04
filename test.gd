c4c57685f33a5cb4ddc378cb0098d5093c8e8335
Tyler Schmidt <47047416+downhillGames@users.noreply.github.com>
2023-02-03 22:29:03 -0800

    WE GOT PLATFORM!

--- test.gd
+++ test.gd
@@ -1,6 +1,10 @@
 extends Node2D
-var newplatform=preload("res://Platform_test.tscn")
-func createNewGoal():
+var newplatform = preload("res://Platform_test.tscn")
+
+
+
+func _ready():
 	var newplatformNode = newplatform.instance()
-	newplatformNode.position = Vector2 (0,0)
+	newplatformNode.position = Vector2 (100,100)
 	self.add_child(newplatformNode)
+

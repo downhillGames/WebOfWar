42e46a915d411db179cff5771835983d87ec5ad7
LSDIZZLE <124414440+LSDIZZLE@users.noreply.github.com>
2023-02-04 00:37:08 -0700

    last upload for night

--- test.gd
+++ test.gd
@@ -5,12 +5,14 @@ var newplatform = preload("res://Platform_test.tscn")
 
 func _ready():
 	for n in range (1,10):
-		newPlatform(n * 100, n * 100)
+		newPlatform(randi() %500, randi() %1000)
 	
 func newPlatform(x, y):
 	var newplatformNode = newplatform.instance()
 	newplatformNode.position = Vector2 (x,y)
-	newplatformNode.position = Vector2 (25,100)
 	self.add_child(newplatformNode)
 
-var rand= randi () %1000
+	var object1 = get_node("Object1")
+	var object2 = get_node("Object2")
+	
+	while object1.position.distance_to(object2.position) < 25
